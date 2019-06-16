205069ea3b038de4b7a191a8430be8404a5d677a
Jan Schiefelbein <jschiefelbein@eonerc.rwth-aachen.de>
2019-06-16 12:03:33 +0200

    Remove long_description

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@
 """
 richardsonpy: Package with Python version of Richardson tool
 
+The Richardson tool is able to generate stochastic occupancy and electric load profiles for residential apartments.
+
 Original version published under GNU GENERAL PUBLIC LICENSE by
 Ian Richardson,
 Murray Thomson and
@@ -17,7 +19,7 @@ UK
 see: https://dspace.lboro.ac.uk/dspace-jspui/handle/2134/3112
 
 Python version provided by:
-Thomas Schütz and
+Thomas Schuetz and
 Jan Schiefelbein
 Institute for Energy Efficient Buildings and Indoor Climate,
 E.ON Energy Research Center,
@@ -26,39 +28,9 @@ RWTH Aachen University
 
 from setuptools import setup
 
-
 setup(name='richardsonpy',
       version='0.2',
       description='Python version of Richardson tool to generate stochastic user and electric load profiles',
-      long_description='''Python version of Richardson tool. 
-
-	  The Richardson tool is able to generate stochastic occupancy and electric load profiles for residential 
-	  apartments.
-
-	  Original version published under GNU GENERAL PUBLIC LICENSE by
-	  Ian Richardson,
-	  Murray Thomson and
-	  David Infield
-	  CREST (Centre for Renewable Energy Systems Technology),
-	  Department of Electronic and Electrical Engineering,
-	  Loughborough University, Leicestershire LE11 3TU, UK
-	  and
-	  Department of Electronic & Electrical Engineering, University of Strathclyde,
-	  UK
-	  Tel. +44 1509 635326. Email address: I.W.Richardson@lboro.ac.uk				
-
-	  see: 
-
-	  https://dspace.lboro.ac.uk/dspace-jspui/handle/2134/3112
-
-	  and
-
-	  https://dspace.lboro.ac.uk/dspace-jspui/handle/2134/5786
-
-	  Python version provided by:
-	  Institute for Energy Efficient Buildings and Indoor Climate,
-	  E.ON Energy Research Center,
-	  RWTH Aachen University''',
       url='https://github.com/RWTH-EBC/richardsonpy',
       author='Institute of Energy Efficient Buildings and Indoor Climate, '
              'E.ON Energy Research Center, RWTH Aachen University',
